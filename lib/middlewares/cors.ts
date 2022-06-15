fea5671ddbdefe181077fe0e1336536bcdd10c1d
Maximo <maximorossini2016@gmail.com>
2022-06-15 17:36:18 -0300

    fixed url typo

--- lib/middlewares/cors.ts
+++ lib/middlewares/cors.ts
@@ -15,7 +15,7 @@ function initMiddleware(middleware) {
 const cors = initMiddleware(
   Cors({
     methods: ["GET", "POST", "OPTIONS"],
-    origin: "http//localhost:3000",
+    origin: "http://localhost:3000",
   })
 );
 
